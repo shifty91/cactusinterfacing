1992ca52317e1724885af2133691b6c7ea32da7a
Kurt Kanzenbach <kurt@kmk-computers.de>
2013-10-11 17:45:22 +0200

    refactered providing of cactus data types
    Macros where used to override these types at compile
    time. Since this method has some disadvantages, typedefs
    are used now.

--- src/include/cctk_Types.h
+++ src/include/cctk_Types.h
@@ -11,32 +11,36 @@
  *
  * CCTK_KEYWORD, CCTK_BOOLEAN got added for parameters.
  */
-#define CCTK_BYTE             unsigned char
-#define CCTK_CHAR             char
-#define CCTK_INT              CCTK_INT4
-#define CCTK_REAL             CCTK_REAL8
-#define CCTK_COMPLEX          CCTK_COMPLEX16
-#define CCTK_POINTER          void *
-#define CCTK_POINTER_TO_CONST const void *
-#define CCTK_STRING           std::string
-/* keyword is just a string with some known values */
-#define CCTK_KEYWORD          std::string
-/* parameters can be BOOLEANs, too */
-#define CCTK_BOOLEAN          bool
+
 
 /*
  * Some of these types are available in different sizes.
  */
-#define CCTK_INT1      int8_t
-#define CCTK_INT2      int16_t
-#define CCTK_INT4      int32_t
-#define CCTK_INT8      int64_t
+typedef int8_t         CCTK_INT1;
+typedef int16_t        CCTK_INT2;
+typedef int32_t        CCTK_INT4;
+typedef int64_t        CCTK_INT8;
+
 /* note: size of these may differ on different architectures */
-#define CCTK_REAL4     float
-#define CCTK_REAL8     double
-#define CCTK_REAL16    long double
-#define CCTK_COMPLEX8  std::complex<CCTK_REAL4>
-#define CCTK_COMPLEX16 std::complex<CCTK_REAL8>
-#define CCTK_COMPLEX32 std::complex<CCTK_REAL16>
+typedef float          CCTK_REAL4;
+typedef double         CCTK_REAL8;
+typedef long double    CCTK_REAL16;
+
+typedef std::complex<CCTK_REAL4>  CCTK_COMPLEX8;
+typedef std::complex<CCTK_REAL8>  CCTK_COMPLEX16;
+typedef std::complex<CCTK_REAL16> CCTK_COMPLEX32;
+
+typedef unsigned char  CCTK_BYTE;
+typedef char           CCTK_CHAR;
+typedef CCTK_INT4      CCTK_INT;
+typedef CCTK_REAL8     CCTK_REAL;
+typedef CCTK_COMPLEX16 CCTK_COMPLEX;
+typedef void *         CCTK_POINTER;
+typedef const void *   CCTK_POINTER_TO_CONST;
+typedef std::string    CCTK_STRING;
+/* keyword is just a string with some known values */
+typedef std::string    CCTK_KEYWORD;
+/* parameters can be BOOLEANs, too */
+typedef bool           CCTK_BOOLEAN;
 
 #endif /* _CCTK_TYPES_H_ */
